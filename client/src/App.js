3b6753504770d3b098d68c9d06e4c0f2c5e00521
Stas Christiansen <schristiansen1@student.gsu.edu>
2017-10-06 20:17:13 -0500

    stashed changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import ScoreInput from './Components/ScoreInput';
+import Header from './Components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">
+            <Header />
             <div className="padding">
-                User: Stas Christiansen
+                <p>User: Stas Christiansen</p>
             </div>
             <ScoreInput />
         </p>
